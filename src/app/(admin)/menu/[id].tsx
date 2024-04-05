34bb35651fe38b5d4a6653e320f0902489db6334
Yaroslav Zaychenko <delflumen@ukr.net>
2024-04-05 11:07:23 +0300

    feat: adjust admin [id] screen

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -41,33 +41,7 @@ const ProductDetailsScreen = () => {
         style={styles.image}
         resizeMode="contain"
       />
-      <Text>Select size</Text>
-      <View style={styles.sizes}>
-        {sizes.map((size) => (
-          <Pressable
-            onPress={() => setSelectedSize(size)}
-            key={size}
-            style={[
-              styles.size,
-              {
-                backgroundColor: selectedSize === size ? "gainsboro" : "white",
-              },
-            ]}
-          >
-            <Text
-              style={[
-                styles.sizeText,
-                { color: selectedSize === size ? "black" : "gray" },
-              ]}
-            >
-              {size}
-            </Text>
-          </Pressable>
-        ))}
-      </View>
-
-      <Text style={styles.price}>${product.price}</Text>
-      <Button onPress={addToCart} text="Add to cart" />
+      <Text style={styles.title}>${product.name}</Text>
     </View>
   );
 };
@@ -86,7 +60,7 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
   },
   title: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "600",
     marginVertical: 10,
   },
@@ -94,23 +68,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: Colors.light.tint,
     fontWeight: "bold",
-    marginTop: "auto",
-  },
-  sizes: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginVertical: 10,
-  },
-  size: {
-    backgroundColor: "gainsboro",
-    width: 50,
-    aspectRatio: 1,
-    borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  sizeText: {
-    fontSize: 20,
-    fontWeight: "500",
   },
 });
