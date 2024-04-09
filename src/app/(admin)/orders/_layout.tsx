73b30b0367ba57b7dcd819a3692a4c60fbfe37db
Yaroslav Zaychenko <delflumen@ukr.net>
2024-04-09 18:27:35 +0300

    feat: add orders toptab navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,16 +1,14 @@
 import Colors from "@/constants/Colors";
 import { FontAwesome } from "@expo/vector-icons";
 import { Link, Stack } from "expo-router";
-import { Pressable } from "react-native";
 
 export default function MenuStack() {
   return (
-    <Stack screenOptions={{}}>
+    <Stack>
       <Stack.Screen
-        name="index"
+        name="list"
         options={{
-          title: "Orders",
-          headerTitleAlign: "center",
+          headerShown: false,
         }}
       />
     </Stack>
--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -1,12 +1,10 @@
 import { FlatList } from "react-native";
-import { Stack } from "expo-router";
 import orders from "@assets/data/orders";
 import OrderListItem from "@/components/OrderListItem";
 
 export default function OrdersScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Orders" }} />
       <FlatList
         data={orders.filter((ord) => !!ord)}
         contentContainerStyle={{ gap: 10, padding: 10 }}
